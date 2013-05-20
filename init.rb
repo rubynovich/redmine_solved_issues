b991e901b076a0bfbd6518645a644953f0ff9c03
rubynovich <roman@shipiev.me>
2013-05-20 15:29:33 +0400

    fix: github -> bitbucket

--- init.rb
+++ init.rb
@@ -1,11 +1,11 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_solved_issues do
-  name 'Redmine Solved Issues plugin'
+  name 'Solved Issues'
   author 'Roman Shipiev'
   description 'Block for my/page and rake-task for mail delivery (IssueStatus = "Solved")'
   version '0.0.1'
-  url 'https://github.com/rubynovich/redmine_solved_issues'
+  url 'https://bitbucket.org/rubynovich/redmine_solved_issues'
   author_url 'http://roman.shipiev.me'
 
   settings :default => {
